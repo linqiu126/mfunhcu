aa63c4f0f8b785b32b8beafb64bc87d848952bc2
unknown <jianlinz@cv0002816n6.ad4.ad.alcatel.com>
2016-06-21 15:41:59 +0800

    zjl@2016/6/21 setup new architeture

--- l1comvm/sysdim.php
+++ l1comvm/sysdim.php
@@ -6,4 +6,13 @@
  * Time: 13:16
  */
 
+//系统任务级MAX_TASK_NUM_IN_ONE_MFUN
+define("MAX_TASK_NUM_IN_ONE_MFUN", 64);
+define("TASK_NAME_MAX_LENGTH", 40);
+
+//消息长度
+define("MSG_NAME_MAX_LENGTH", 70);
+define("MAX_MFUN_MSG_BODY_LENGTH", 400);
+define("MFUN_FILE_NAME_LENGTH_MAX", 100);
+
 ?>
