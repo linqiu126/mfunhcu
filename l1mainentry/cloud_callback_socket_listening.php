9a40bf298e4e35c78514b0fae547483aa3b37ccc
linqiu126 <linqiu126@gmail.com>
2016-09-17 14:47:53 +0800

    20160917 QL, swoole heartbeat para adjust

--- l1mainentry/cloud_callback_socket_listening.php
+++ l1mainentry/cloud_callback_socket_listening.php
@@ -21,8 +21,8 @@ class classL1MainEntrySocketListenServer
             'daemonize' => true,
             //'log_file' => '/home/hitpony/swooleserver.log', //vmware环境使用
             'log_file' => '/home/qiulin/swooleserver.log', //爱启云环境使用
-            'heartbeat_idle_time' => 60, //只设置heartbeat_idle_time，未设置heartbeat_check_interval，底层将不会创建心跳检测线程，PHP代码中未来可以调用heartbeat方法手工处理超时的连接
-            //'heartbeat_check_interval' => 6, //每6秒遍历一次
+            'heartbeat_idle_time' => 600, //只设置heartbeat_idle_time，未设置heartbeat_check_interval，底层将不会创建心跳检测线程，PHP代码中未来可以调用heartbeat方法手工处理超时的连接
+            'heartbeat_check_interval' => 60, //每6秒遍历一次
             'max_request' => 10000,
             'dispatch_mode' => 2,
             'debug_mode'=> 1,
