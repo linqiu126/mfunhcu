7718abfb02fe749c778c07756780ed546d93c3d2
liuzehong <liuzehong@hotmail.com>
2016-09-24 15:05:19 +0800

    LZH@2016/09/24 fix bug

--- l4oamtools/tool.create.menu.php
+++ l4oamtools/tool.create.menu.php
@@ -44,20 +44,20 @@ if (MFUN_WX_APPID == "wx1183be5c8f6a24b4") //如果是测试号
 {
     $self_create_menu =
     '{"button":[
-                {"name":"调测工具",
+                {"name":"调测",
                     "sub_button":[
                                   {"type":"click","name":"强制绑定","key":"CLICK_BIND"},
                                   {"type":"click","name":"绑定查询","key":"CLICK_BIND_INQ"},
                                   {"type":"click","name":"解绑自己","key":"CLICK_UNBIND"}]
                 },
-                {"name":"功能设置",
+                {"name":"设置",
                     "sub_button":[
                                   {"type":"click","name":"周期读取(开)","key":"CLICK_EMC_PERIOD_READ_OPEN"},
                                   {"type":"click","name":"周期读取(关)","key":"CLICK_EMC_PERIOD_READ_CLOSE"}，
                                   {"type":"click","name":"Trace开","key":"CLICK_TRACE_ON"},
                                   {"type":"click","name":"Trace关","key":"CLICK_TRACE_OFF"}]
                 },
-                {"name":"生产工具",
+                {"name":"生产",
                      "sub_button":[{"type":"scancode_push","name":"扫码绑定","key":"QR_SCAN"},
                                    {"type":"click","name":"电量查询","key":"CLICK_POWER_STATUS"},
                                    {"type":"click","name":"瞬时读取","key":"CLICK_EMC_INSTANT_READ"}]
