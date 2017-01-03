1bef2e922a5f3c148eb821d37f7c4a2055f23b0f
zehongliu <liuzehong@hotmail.com>
2017-01-03 20:07:18 +0800

    LZH@2016/01/03 fix bug of FHYS wechat UI

--- l4fhyswechat/request.php
+++ l4fhyswechat/request.php
@@ -2,8 +2,18 @@
 include_once "../l1comvm/vmlayer.php";
 require_once "dbi_l4fhys_wechat.class.php";
 
-//L4CLOUDLOCK的入口起点
-//这里的入参格式是跟前端界面商量约定好的
+//L4FHYS Wechat的入口起点
+//这里调用h5ui_entry_fhys_wechat是为了保证所有的入口都在/l1mainentry下面
+
+$request_body = file_get_contents('php://input');
+//echo $request_body;
+$payload = json_decode($request_body,true);
+//echo $payload;
+if (isset($payload["action"])) $_GET["action"] = trim($payload["action"]); else $_GET["action"] = "";
+if (isset($payload["type"])) $_GET["type"] = trim($payload["type"]); else $_GET["type"] = "";
+if (isset($payload["user"])) $_GET["user"] = trim($payload["user"]); else $_GET["user"] = "";
+if (isset($payload["body"])) $_GET["body"] = $payload["body"]; else $_GET["body"] = "";
+
 if (isset($_GET["action"])){
     require("../l1mainentry/h5ui_entry_fhys_wechat.php");
 }
