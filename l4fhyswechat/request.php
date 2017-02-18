63ac0c3a221f4af4fa6fcd43c12d6e925a4dc9eb
zehongliu <liuzehong@hotmail.com>
2017-02-18 21:25:57 +0800

    LZH@2017/02/18 update FHYS wechat request

--- l4fhyswechat/request.php
+++ l4fhyswechat/request.php
@@ -5,7 +5,6 @@ require_once "dbi_l4fhys_wechat.class.php";
 //L4FHYS Wechat的入口起点
 //这里调用h5ui_entry_fhys_wechat是为了保证所有的入口都在/l1mainentry下面
 
-/*
 $request_body = file_get_contents('php://input');
 //echo $request_body;
 $payload = json_decode($request_body,true);
@@ -14,7 +13,6 @@ if (isset($payload["action"])) $_GET["action"] = trim($payload["action"]); else 
 if (isset($payload["type"])) $_GET["type"] = trim($payload["type"]); else $_GET["type"] = "";
 if (isset($payload["user"])) $_GET["user"] = trim($payload["user"]); else $_GET["user"] = "";
 if (isset($payload["body"])) $_GET["body"] = $payload["body"]; else $_GET["body"] = "";
-*/
 
 if (isset($_GET["action"])){
     require("../l1mainentry/h5ui_entry_fhys_wechat.php");
