12f90d810e8607286103ae0630d4c3b4dd3fcd83
zehongliu <liuzehong@hotmail.com>
2017-03-07 22:21:09 +0800

    LZH@2017/03/07 fix FHYS wechat map GPS accuracy

--- l4oamtools/hcuinstall/request.php
+++ l4oamtools/hcuinstall/request.php
@@ -1,6 +1,6 @@
 <?php
 
-//include_once "../../l1comvm/vmlayer.php";
+include_once "../../l1comvm/vmlayer.php";
 header("Content-type:text/html;charset=utf-8");
 function _getfilecounts($ff){
     if(!file_exists($ff)) return 0;
