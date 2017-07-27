2da0ea50332621a0c9c74087c36125cc61d281a4
zehongliu <liuzehong@hotmail.com>
2017-07-27 18:26:09 +0800

    LZH@2017/07/27 update CCL auth for test

--- l2sensorproc/sensorccl/dbi_l2snr_ccl.class.php
+++ l2sensorproc/sensorccl/dbi_l2snr_ccl.class.php
@@ -148,12 +148,13 @@ class classDbiL2snrCcl
                 {
                     case MFUN_L3APL_F2CM_AUTH_TYPE_NUMBER:
                         //防止用户重复点击，对于用户名开锁，只保留一次开锁
+                        /*
                         if($validnum > 0){
                             $query_str = "DELETE FROM `t_l3f2cm_fhys_keyauth` WHERE (`sid` = '$sid') ";
                             $resp = $mysqli->query($query_str);
                             $auth_check = true;
                         }
-                        /*
+                        */
                         $remain_validnum = $validnum - 1;
                         if ($remain_validnum == 0){
                             $query_str = "DELETE FROM `t_l3f2cm_fhys_keyauth` WHERE (`sid` = '$sid') ";
@@ -164,7 +165,7 @@ class classDbiL2snrCcl
                             $query_str = "UPDATE `t_l3f2cm_fhys_keyauth` SET  `validnum` = '$remain_validnum' WHERE (`sid` = '$sid')";
                             $resp = $mysqli->query($query_str);
                             $auth_check = true;
-                        }*/
+                        }
                         break;
                     case MFUN_L3APL_F2CM_AUTH_TYPE_TIME:
                         $timestamp = time();
